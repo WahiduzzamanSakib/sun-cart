c30ab127e10edbc7b25b1fa385ec6045affd4319
WahiduzzamanSakib <wahidzamanpg@gmail.com>
2026-05-04 14:22:20 +0600

    Products  Add

--- src/app/components/PopularProducts.jsx
+++ src/app/components/PopularProducts.jsx
@@ -11,12 +11,12 @@ const PopularProducts = async () => {
     const topData = data.sort((a, b) => b.rating - a.rating).slice(0, 3);
 
     return (
-        <div>
+        <div className='bg-blue-100 p-3'>
             <h1 className="text-3xl font-bold my-5 text-center">
                 Popular Products
             </h1>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 container mx-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 container mx-auto ">
                 {topData.map(item => (
                     <ProductsCard key={item.id} data={item} />
                 ))}
---
+++ src/app/products/page.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import ProductsCard from '../components/ProductsCard';
+
+const Products = async () => {
+    const res = await fetch("https://sun-cart-c2md.vercel.app/data.json", {
+        cache: "no-store",
+    });
+    const item = await res.json();
+   
+
+    return (
+        <div className='bg-blue-200 p-2'>
+            <h2 className="text-3xl font-bold my-5 text-center">Products</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 container mx-auto">
+                {item.map(item => (
+                    <ProductsCard key={item.id} data={item} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Products;
